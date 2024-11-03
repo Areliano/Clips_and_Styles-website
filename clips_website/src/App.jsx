7ed7675bbac2a307660d1c34ab14ae9c96df2fdf
Your Name <mmogire11@gmail.com>
2024-11-03 17:55:36 +0300

    The following have been added:
    - Tailwind css
    - React-router-dom
    - Configured routes for homepage and the about page
    - components and pages folders
    - Setup code from previous Clips and styles Navbar

--- clips_website/src/App.jsx
+++ clips_website/src/App.jsx
@@ -1,1 +1,19 @@
- 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
