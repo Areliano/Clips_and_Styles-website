3ebcf11a419f435321827a5e1a72d33b38b4ca99
Your Name <mmogire11@gmail.com>
2024-11-04 14:50:49 +0300

    Monday - Added meta tags for keywords and descriptions for clips and styles - Cleaned up index.css

--- clips_website/src/components/Navbar.jsx
+++ clips_website/src/components/Navbar.jsx
@@ -9,20 +9,19 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="navigation-wrapper w-[100%]  sticky top-0 z-10 backdrop-blur-lg ">
+    <nav className="navigation-wrapper w-[100%]  sticky top-0 z-10 backdrop-blur-lg bg-white border-b border-neutral-100">
       <div className="p-3 h-[60px] flex items-center justify-between w-full container mx-auto">
         <div className="logo">
-          <NavLink to="/" className="text-lg tracking-wider">
+          <NavLink to="/home" className="text-lg tracking-wider">
             Clips & Styles
           </NavLink>
         </div>
 
         <div className="right-nav flex items-center justify-between gap-4">
-            
           {!isOpen ? (
             <button
               type="button"
-              className="menubar flex flex-col items-end gap-1 border border-slate-100/40 p-2 rounded-md"
+              className="menubar flex flex-col items-end gap-1 px-2 rounded-md active:translate-y-[2px]"
               onClick={toggleMenu}
             >
               <div className="line bg-black rounded-lg h-[2px] w-[21px]"></div>
@@ -32,7 +31,7 @@ export default function Navbar() {
           ) : (
             <button
               type="button"
-              className="menubar flex flex-col items-end gap-1 border border-slate-100/40 p-2 rounded-md text-5xl font-light"
+              className="menubar px-2 rounded-md text-5xl font-light hover:bg-[whitesmoke] active:translate-y-[2px]"
               onClick={toggleMenu}
             >
               &times;
@@ -43,9 +42,10 @@ export default function Navbar() {
           {isOpen ? (
             <section
               id="menu-content"
-              className="w-full absolute top-[3.75rem] left-0 p-2  bg-white"
+              className="w-full min-h-[95dvh] absolute top-[3.75rem] left-0 p-2 bg-white z-10"
+              onClick={toggleMenu}
             >
-              <div className="content divide-y divide-gray-200 text-black flex flex-col container mx-auto">
+              <div className="content divide-y divide-neutral-100 text-black flex flex-col container mx-auto bg-white border border-neutral-100">
                 <NavLink
                   to="/home"
                   onClick={toggleMenu}
@@ -62,23 +62,43 @@ export default function Navbar() {
                   About Clips & Styles
                 </NavLink>
 
-                <NavLink to="/our-story" onClick={toggleMenu} className="content-items">
+                <NavLink
+                  to="/our-story"
+                  onClick={toggleMenu}
+                  className="content-items"
+                >
                   Our Story
                 </NavLink>
 
-                <NavLink to="/our-manifesto" onClick={toggleMenu} className="content-items">
+                <NavLink
+                  to="/our-manifesto"
+                  onClick={toggleMenu}
+                  className="content-items"
+                >
                   Our Manifesto
                 </NavLink>
 
-                <NavLink to="/our-team" onClick={toggleMenu} className="content-items">
+                <NavLink
+                  to="/our-team"
+                  onClick={toggleMenu}
+                  className="content-items"
+                >
                   Our Team
                 </NavLink>
 
-                <NavLink to="/for-customers" onClick={toggleMenu} className="content-items">
+                <NavLink
+                  to="/for-customers"
+                  onClick={toggleMenu}
+                  className="content-items"
+                >
                   Clips & Styles for customers
                 </NavLink>
 
-                <NavLink to="/for-shop-owners" onClick={toggleMenu} className="content-items">
+                <NavLink
+                  to="/for-shop-owners"
+                  onClick={toggleMenu}
+                  className="content-items"
+                >
                   Clips & Styles for beauty Shop owners
                 </NavLink>
               </div>
