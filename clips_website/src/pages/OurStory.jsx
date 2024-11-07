076830422555364503a837ef50c8a8a72becf632
Fatma <fatmahussein355@gmail.com>
2024-11-07 15:55:52 +0300

    Our Story changes

--- clips_website/src/pages/OurStory.jsx
+++ clips_website/src/pages/OurStory.jsx
@@ -11,21 +11,21 @@ export default function OurStory() {
         {/* First Paragraph */}
         <div className="text-lg">
           <p>
-          The story of Clips&Styles is rooted in the everyday experience of customers who have faced the common frustration of the beauty industry. Imagine those moments when you needed a quick trim before an important meeting or wanted the perfect hairstyle for a specific event,only to eal with the long waits, missed appointments or unreliable service. these real-life struggles inspired the creation of Clips&Styles, with a vision to transform the way people experience beauty services in Kenya.
+          The story of Clips&Styles is deeply rooted in the everyday experience of customers who have faced the common frustration of the beauty industry. Imagine those moments when you needed a quick trim before an important meeting or wanted the perfect hairstyle for a specific event,only to eal with the long waits, missed appointments or unreliable service. these real-life struggles inspired the creation of Clips&Styles, with a vision to transform the way people experience beauty services in Kenya.
           </p>
 
         </div>
         {/* Second Paragraph */}
         <div className="text-lg">
           <p>
-          As customer, you have likely encountered the hassle of finding a trusted stylist, struggled to book appointment that fit your busy schedule or settled for less -than-ideal service because of limited options. Clips&Styles was founded to solve these challenges offering a platform that connects you with vetted beauty experts near you , making it easier to find the right service provider at the right time.
+          As a customer, you have likely encountered the hassle of finding a trusted stylist, struggled to book appointment that fit your busy schedule or settled for less -than-ideal service because of limited options. Clips&Styles was founded to solve these challenges offering a platform that connects you with vetted beauty experts near you , making it easier to find the right service provider at the right time.
           </p>
         </div>
 
         {/* Third Paragraph */}
         <div className="text-lg">
           <p>
-          As beauty shop owner, drawing from the real -life experience in running a beauty shop , our co-founder understood the struggle of balancing daily operations with the need to attract new clients. With Clips&Styles you can manage your booking ore effectively , reduce downtime and showcase your skills to a wider audience. The platform provides the tools needed to grow your business including appointment scheduling, client management and promotional features to highlight your unique services. 
+          As a beauty shop owner, drawing from the real -life experience in running a beauty shop , our co-founder understood the struggle of balancing daily operations with the need to attract new clients. With Clips&Styles you can manage your booking ore effectively , reduce downtime and showcase your skills to a wider audience. The platform provides the tools needed to grow your business including appointment scheduling, client management and promotional features to highlight your unique services. 
           </p>
         </div>
 
